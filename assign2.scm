6fa9b8e0d26c6fad662347cf4d30d43042c5ca1c
Thomas Willingham <thomas@tshows.us>
2016-09-28 17:40:22 -0500

    runs

---
+++ assign2.scm
@@ -0,0 +1,33 @@
+(define (author)
+    (println "AUTHOR: Thomas Willingham twillingham@crimson.ua.edu")
+    )
+
+(define (run1)
+  )
+
+(define (run2)
+  )
+
+(define (run3)
+  )
+
+(define (run4)
+  )
+
+(define (run5)
+  )
+
+(define (run6)
+  )
+
+(define (run7)
+  )
+
+(define (run8)
+  )
+
+(define (run9)
+  )
+
+(define (run10)
+  )
