d7897a63345b107e91c3c9464cc07e7349aaa145
Thomas Willingham <thomas@tshows.us>
2016-10-15 15:22:32 -0500

    stack

--- assign2.scm
+++ assign2.scm
@@ -3,19 +3,62 @@
     )
 
 (define (iterator # $x items $)
-    (define (f) 
-        (lambda (x)
-            (while (valid? $)
-                   (car $)
-                   )
-            )
+    (define (join holder items $)
+        (if (valid? car(x)))
+            
         )
     )
 
+(define (for-loop arglist procedure)
+  (cond
+    ((null? arglist) nil)
+    (else (begin
+      (procedure (car arglist))
+      (for-loop (cdr arglist) procedure)
+      ))
+    )
+  )
+
+(define (for-loop arg procedureList finalList)
+  (cond
+    ((null? procedureList) (println finalList))
+    (else (begin
+      (for-loop arg (cdr procedureList) (cons arg (car procedureList)))
+      ))
+    )
+  )
 
 
 
+(for-loop 1  (list (lambda (x) (inspect x))) (list ))
 
+(define (push s x)
+    (if (null? s)
+        (list x)
+        (append (list x) s)
+        )
+    )
+
+(define (pop s)
+    (if (null? s)
+        '()
+        (cdr s)
+        )
+    )
+
+(define (speek s)
+    (if (null? s)
+        '()
+        (car s)
+        )
+    )
+
+(define (ssize s)
+    (if (null? s)
+        0
+        (length s)
+        )
+    )
 
 (define (run1)
   )
